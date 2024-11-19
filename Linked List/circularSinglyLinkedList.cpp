c4392872a4987dd01f5124c0c316609f56b8494e
Akshat <akshatsahay19@gmail.com>
2024-11-20 02:23:44 +0530

    Circular Linked List

--- Linked List/circularSinglyLinkedList.cpp
+++ Linked List/circularSinglyLinkedList.cpp
@@ -5,38 +5,38 @@ class Node{
     public:
         int val;
         Node *next;
-        Node *prev;
 
-    Node(int val){
-        this->val = val;
-        this->next = NULL;
-        this->prev = NULL;
-    }
+        Node(int val){
+            this->val = val;
+            this->next = NULL;
+        }
 };
 
-class DoublyLinkedList{
+class CircularSinglyLinkedList{
     private:
         Node *head = NULL;
     
     public:
         int getLength(){
             int len = 0;
+            if(!head){
+                return 0;
+            }
             Node *helper = head;
-            while(helper){
+            do{
                 helper = helper->next;
                 len++;
-            }
+            } while (helper != head);
             return len;
         }
 
         void printList(){
-            cout << "Doubly Linked List: ";
             Node *helper = head;
-            while(helper){
-                cout << helper->val << " -> ";
+            while(helper->next!=head){
+                cout << helper->val << "->";
                 helper = helper->next;
             }
-            cout << "NULL" << endl;
+            cout << "HEAD" << endl;
         }
 
         void insertion(int pos, int data){
@@ -45,26 +45,28 @@ class DoublyLinkedList{
                 return;
             }
             Node *newNode = new Node(data);
+            Node *helper = head;
             if(pos == 0){
-                newNode->next = head;
-                if(head){
-                    head->prev = newNode;
+                if(!head){
+                    head = newNode;
+                    head->next = head;
+                }else{
+                    newNode->next = head;
+                    do{
+                        helper = helper->next;
+                    } while (helper != head);
+                    helper->next = newNode;
+                    head = newNode;
                 }
-                head = newNode;
+                return;
             }else{
                 int index = 1;
-                Node *helper = head;
-                while(pos<index){
-                    pos++;
-                    helper = helper->next;
-                }
-
-                newNode->next = helper->next;
-                if(helper->next){
-                    helper->next->prev = newNode;
-                }
-                newNode->prev = helper;
-                helper->next = newNode;
+                    while(index<pos){
+                        index++;
+                        helper = helper->next;
+                    }
+                    newNode->next = helper->next;
+                    helper->next = newNode;
             }
             cout << "Insertion done sucessfully." << endl;
         }
@@ -75,41 +77,43 @@ class DoublyLinkedList{
                 return;
             }
 
+            Node *helper = head;
             Node *toBeDeleted;
+
             if(pos == 0){
                 toBeDeleted = head;
-                head = head->next;
-                if(head){
-                    head->prev = NULL;
+                if(head->next == head){
+                    head = NULL;
+                }else{
+                    while(helper->next != head){
+                        helper = helper->next;
+                    }
+                    
+                    helper->next = helper->next->next;
+                    head = helper->next;
+                    free(toBeDeleted);
                 }
-                free(toBeDeleted);
-                cout << "Deletion done sucessfully." << endl;
                 return;
+            }else{
+                int index = 1;
+                while(index<pos){
+                    helper = helper->next;
+                    index++;
+                }
+                toBeDeleted = helper->next;
+                helper->next = helper->next->next;
+                free(toBeDeleted);
             }
-
-            int index = 1;
-            Node *helper = head;
-            while(index<pos){
-                index++;
-                helper = helper->next;
-            }
-
-            toBeDeleted = helper->next;
-            helper->next = helper->next->next;
-            if(helper->next){
-                helper->next->prev = helper;
-            }
-            free(toBeDeleted);
+            cout << "Deletion done sucessfully." << endl;
         }
 
         void updation(int pos, int data){
-            if(pos<0 || pos>getLength()){
+            if(pos<0 || pos>=getLength()){
                 cout << "Invalid Index" << endl;
                 return;
             }
-
             Node *helper = head;
-            int index = 1;
+            int index = 0;
             while(index<pos){
                 index++;
                 helper = helper->next;
@@ -129,7 +133,7 @@ class DoublyLinkedList{
 };
 
 int main(){
-    DoublyLinkedList list;
+    CircularSinglyLinkedList list;
     
     int n;
     do{
