5afbc6bab87e1b99a88ee86d3b9b1649f1ee9111
Akshat <akshatsahay19@gmail.com>
2025-12-18 23:02:05 +0530

    LCS in binary tree

---
+++ 08-Tree/LCSinBinaryTree.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return NULL;
+        if(root == p || root == q) return root;
+
+        TreeNode* leftSide = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightSide = lowestCommonAncestor(root->right, p, q);
+
+        if(leftSide && rightSide) return root;
+        if(leftSide) return leftSide;
+        if(rightSide) return rightSide;
+        return NULL;
+    }
+};
+
+int main() {
+    /*
+            3
+           / \
+          5   1
+         / \ / \
+        6  2 0  8
+           / \
+          7   4
+    */
+
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(1);
+    root->left->left = new TreeNode(6);
+    root->left->right = new TreeNode(2);
+    root->right->left = new TreeNode(0);
+    root->right->right = new TreeNode(8);
+    root->left->right->left = new TreeNode(7);
+    root->left->right->right = new TreeNode(4);
+
+    Solution sol;
+
+    TreeNode* p = root->left;                 // node 5
+    TreeNode* q = root->left->right->right;   // node 4
+
+    TreeNode* ans = sol.lowestCommonAncestor(root, p, q);
+
+    if(ans)
+        cout << "LCA: " << ans->val << endl;
+    else
+        cout << "LCA not found" << endl;
+
+    return 0;
+}
